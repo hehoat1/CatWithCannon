8dfa9ff734d1a06322f31ddc55d043ab2b6d4910
Noah Dinan <noah.dinan@gmail.com>
2022-05-31 04:12:38 -0500

    Organized(kinda)
    everything works
    yassss.
    
    Co-Authored-By: hehoat1 <106416464+hehoat1@users.noreply.github.com>

---
+++ Assets/Player/Cannon/Cannonballs/Base/BallShoot.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float projectileSpeed = 30f;
+    public float shotDelay;
+    public float fireRate = 0.5f;
+    private float nextFire = 0.0f;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            Shoot();
+        }
+
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * projectileSpeed, ForceMode2D.Impulse);
+    }
+}
