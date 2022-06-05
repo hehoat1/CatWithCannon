6030fad90d5f400db722ad7e4ffad60fed471b34
Noah Dinan <noah.dinan@gmail.com>
2022-06-05 15:32:06 -0500

    added ignoring bullet collisions with cat

--- Assets/Player/Cannon/Cannonballs/Base/BallCollide.cs
+++ Assets/Player/Cannon/Cannonballs/Base/BallCollide.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class BallCollide : MonoBehaviour
 {
-    public GameObject cat;
     public GameObject hitEffect;
     public GameObject bullet;
 
 
     void Start()
     {
-        Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        Physics2D.IgnoreLayerCollision(10, 13, true);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
